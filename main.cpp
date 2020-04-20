aa2d1f54bfa3cc34e46eee9d6340df643806875e
alvaro aguirre <44936171+chelaschill@users.noreply.github.com>
2020-04-20 10:50:56 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,14 +5,22 @@ using namespace std;
 
 int main() {
 
-    int *array =  new int[5];
+    int array[5] =  {1,2,3,4,5};
 
-    for(int i = 0; i < sizeof(array); i++)
-        array[i] = i;
 
-    List<int> lista(array);
+    List<int> lista;
+    auto nodo = new Node<int>(4, nullptr);
+    lista.push_back(1);
+    lista.push_back(2);
+    lista.push_back(3);
+    //lista.push_back(4);
+    lista.push_back(5);
 
-    //lista.List(array);
+    lista.insert(nodo, 3);
+
+   cout << lista;
+
+
 
 
     return 0;
