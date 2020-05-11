8fe9f522c907e1989bb637614a11c31347c402f8
alvaro aguirre <44936171+chelaschill@users.noreply.github.com>
2020-05-10 23:16:53 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,21 @@
 #include <iostream>
-#include "List.h"
+#include "traits.cpp"
 
 using namespace std;
 
 int main() {
 
-    int array[5] =  {1,2,3,4,5};
+    Double_Linked_Circular_List<int> dcl;
 
+    dcl.push_back(1);
+    dcl.push_back(5);
+    dcl.push_back(2);
+   
 
-    List<int> lista;
-    auto nodo = new Node<int>(4, nullptr);
-    lista.push_back(1);
-    lista.push_back(2);
-    lista.push_back(3);
-    //lista.push_back(4);
-    lista.push_back(5);
+    dcl.pop_front();
+    dcl.pop_back();
 
-    lista.insert(nodo, 3);
-
-   cout << lista;
-
+    cout << dcl;
 
 
 
